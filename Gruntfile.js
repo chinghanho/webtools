2e125bd2cdf27cdd3a473d9a5c57c9c6990b42c6
CHH <chh@chh.tw>
2013-08-27 22:41:32 +0800

    Move singleRun option to configFile
    Keep Gruntfile clean and simple… :p

--- Gruntfile.js
+++ Gruntfile.js
@@ -290,8 +290,7 @@ module.exports = function (grunt) {
     },
     karma: {
       unit: {
-        configFile: 'karma.conf.js',
-        singleRun: true
+        configFile: 'karma.conf.js'
       }
     },
     cdnify: {
