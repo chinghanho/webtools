96024db187f9b79351b059eed2ed31597c461f26
CHH <chh@chh.tw>
2013-08-28 23:30:36 +0800

    Add E2E test and fix karma-e2e.conf.js syntax bug.

--- Gruntfile.js
+++ Gruntfile.js
@@ -291,6 +291,9 @@ module.exports = function (grunt) {
     karma: {
       unit: {
         configFile: 'karma.conf.js'
+      },
+      e2e: {
+        configFile: 'karma-e2e.conf.js'
       }
     },
     cdnify: {
