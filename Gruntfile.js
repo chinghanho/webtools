3a9eaed326c9dd55dd4c9c60410410e0895a0afb
CHH <chh@chh.tw>
2013-08-28 15:36:56 +0800

    Delete open command from task of grunt server.
    Vagrant can't open browser in VM.

--- Gruntfile.js
+++ Gruntfile.js
@@ -321,14 +321,13 @@ module.exports = function (grunt) {
 
   grunt.registerTask('server', function (target) {
     if (target === 'dist') {
-      return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
+      return grunt.task.run(['build', 'connect:dist:keepalive']);
     }
 
     grunt.task.run([
       'clean:server',
       'concurrent:server',
       'connect:livereload',
-      'open',
       'watch'
     ]);
   });
