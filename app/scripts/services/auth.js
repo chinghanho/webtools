5748d1f0bfec5fadbd7c34f5eb14ed92e034b778
CHH <chh@chh.tw>
2013-08-31 21:23:57 +0800

    Add admin condition to determine user is admin or not.

--- app/scripts/services/auth.js
+++ app/scripts/services/auth.js
@@ -8,15 +8,20 @@ angular.module('chhResourcesApp')
       user: {},
 
       isLogged: false,
+      isAdmin: false,
 
       signIn: function() {
         $http.get('/api/sessions/check')
           .success(function(user) {
             Auth.isLogged = true;
             Auth.user = user;
+            if (user.role == 'admin') {
+              Auth.isAdmin = true;
+            }
           })
           .error(function(result, status) {
             Auth.isLogged = false;
+            Auth.isAdmin = false;
             Auth.user = null;
           });
       },
