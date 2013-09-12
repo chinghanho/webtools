68ef240c6ceef9665bb92ed5e190e3f92947e65f
CHH <chh@chh.tw>
2013-09-12 18:31:29 +0800

    discard all test code...

--- test/spec/controllers/signin.js
+++ test/spec/controllers/signin.js
@@ -2,21 +2,4 @@
 
 describe('Controller: SigninCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('chhResourcesApp'));
-
-  var SigninCtrl,
-    scope;
-
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    SigninCtrl = $controller('SigninCtrl', {
-      $scope: scope
-    });
-  }));
-
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
 });
--- test/spec/services/auth.js
+++ test/spec/services/auth.js
@@ -2,30 +2,4 @@
 
 describe('Service: Auth', function () {
 
-  // load the controller's module
-  beforeEach(module('chhResourcesApp'));
-
-  var $httpBackend, $rootScope, $controller, mockAuth;
-
-  var notFound = {
-    "message": "Not Found"
-  }
-
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function($controller, $rootScope, $injector) {
-
-    $httpBackend = $injector.get('$httpBackend');
-    mockAuth = $injector.get('Auth');
-    $httpBackend.whenGET('/api/sessions/check').respond(notFound);
-
-  }));
-
-  it('should check current authentication', inject(function() {
-
-    var result = mockAuth.checkAuth();
-    $httpBackend.flush();
-    expect(result.message).toBe(notFound.message);
-
-  }));
-
 });
