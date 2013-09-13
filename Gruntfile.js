3c82616b3964b441878c196d53f8db621e680bc9
CHH <chh@chh.tw>
2013-09-13 11:07:35 +0800

    Delete public dir.

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,8 +66,7 @@ module.exports = function (grunt) {
             return [
               lrSnippet,
               mountFolder(connect, '.tmp'),
-              mountFolder(connect, yeomanConfig.app),
-              mountFolder(connect, 'public')
+              mountFolder(connect, yeomanConfig.app)
             ];
           }
         }
