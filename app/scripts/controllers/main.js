f448e85fd0d32400d6a102d2144c6a10d73a60f9
CHH <chh@chh.tw>
2013-09-15 21:42:02 +0800

    修正了 modal 顯示不出來的問題。

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,4 +16,54 @@ angular.module('chhResourcesApp')
     $rootScope.auth = Auth;
     $rootScope.search = {};
 
+    /**
+     * Modal Control
+     */
+
+    $scope.showModal = false;
+    $scope.modal = function(arg) {
+
+      if (arg == 'newResource') {
+        if ($rootScope.auth.isLogged) {
+          $scope.modalContent = 'newResource';
+          $scope.showModal = true;
+        }
+        else {
+          $scope.modalContent = 'signIn';
+          $scope.showModal = true;
+        }
+      }
+      else if (arg == 'signIn') {
+        $scope.modalContent = 'signIn';
+        $scope.showModal = true;
+      }
+      else if (arg == 'signUp') {
+        $scope.showModal = true;
+        $scope.modalContent = 'signUp';
+      }
+      else if (arg) {
+        clearModalContent();
+        $scope.showModal = true;
+        $scope.modalResource = arg;
+      }
+      else {
+        clearModalContent();
+        $scope.showModal = false;
+      }
+
+      if ($scope.showModal) {
+        $('body').addClass('modal-enabled');
+        $('.modal__overlay').addClass('modal-enabled');
+      }
+      else {
+        $('body').removeClass('modal-enabled');
+        $('.modal__overlay').removeClass('modal-enabled');
+      }
+
+    }
+
+    function clearModalContent() {
+      $scope.modalContent = null;
+    }
+
   });
--- app/scripts/controllers/resources.js
+++ app/scripts/controllers/resources.js
@@ -57,52 +57,6 @@ angular.module('chhResourcesApp')
       $scope.resourcesContent = null;
     }
 
-    /**
-     * Modal Control
-     */
-
-    $scope.showModal = false;
-    $scope.modal = function(arg) {
-
-      if (arg == 'newResource') {
-        if ($rootScope.auth.isLogged) {
-          $scope.modalContent = 'newResource';
-          $scope.showModal = true;
-        }
-        else {
-          $scope.modalContent = 'signIn';
-          $scope.showModal = true;
-        }
-      }
-      else if (arg == 'signIn') {
-        $scope.modalContent = 'signIn';
-        $scope.showModal = true;
-      }
-      else if (arg == 'signUp') {
-        $scope.showModal = true;
-        $scope.modalContent = 'signUp';
-      }
-      else if (arg) {
-        clearModalContent();
-        $scope.showModal = true;
-        $scope.modalResource = arg;
-      }
-      else {
-        clearModalContent();
-        $scope.showModal = false;
-      }
-
-      if ($scope.showModal) {
-        $('body').addClass('modal-enabled');
-        $('.modal__overlay').addClass('modal-enabled');
-      }
-      else {
-        $('body').removeClass('modal-enabled');
-        $('.modal__overlay').removeClass('modal-enabled');
-      }
-
-    }
-
     // $scope.clickUploader = function() {
     //   $('#cover-image-uploader').click();
     // }
@@ -150,10 +104,6 @@ angular.module('chhResourcesApp')
     /**
      * Private functions
      */
-
-    function clearModalContent() {
-      $scope.modalContent = null;
-    }
 
     function clearModelValues(model) {
       for (var i in model) {
